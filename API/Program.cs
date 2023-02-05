48e058608eb615ef53d7c930cb1c6fca022aa54b
Deepia <30970811+Deepia@users.noreply.github.com>
2023-02-05 18:28:31 +0530

    05-02-23

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(
     option=>option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
@@ -25,9 +28,20 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope(); 
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try{
+     await context.Database.MigrateAsync();
+}
+catch(Exception ex){
+  logger.LogError(ex,"An error occured during migration.");
+}
 
 app.Run();
